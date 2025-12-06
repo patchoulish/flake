a9df5fd00ec6e909108cf34a221ff6bc68bd91bb
patchouli <self@patchouli.sh>
2025-12-06 14:01:46 -0500

    Fix build failure on NixOS

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -3,6 +3,9 @@
 	home-manager = {
 		extraSpecialArgs = { inherit self self' inputs inputs'; };
 
+		useGlobalPkgs = true;
+		useUserPackages = true;
+
 		# The file extension to use when Home Manager overwrites an existing file.
 		backupFileExtension = "backup";
 
